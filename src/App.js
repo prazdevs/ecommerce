ad3461239904901c2b4743ddd60cd12b2c21f956
PraZ <sachabouillez@gmail.com>
2020-01-13 00:21:51 +0100

    sign in page and form basic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import Header from './components/header/header.component';
 
 import './App.css';
@@ -13,7 +15,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage}></Route>
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
