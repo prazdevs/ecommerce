dc7444e4c8e21c5a38a5050197ff91b1050b19cf
PraZ <sachabouillez@gmail.com>
2020-01-12 02:26:10 +0100

    base structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
-  return <div>Hello world</div>;
+  return <HomePage />;
 }
 
 export default App;
