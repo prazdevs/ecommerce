adb9c2f7394f09eab44a259ff9435205f99661c5
PraZ <sachabouillez@gmail.com>
2020-01-12 22:06:31 +0100

    updating shop page and collection preview components and items

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
 function App() {
-  return <HomePage />;
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage}></Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
