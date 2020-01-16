eb9ccdea5f3ac21dead868c298615f9c81338b81
Sacha Bouillez <sachabouillez@gmail.com>
2020-01-16 13:19:09 +0100

    new redux flow for shop directory

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,11 +6,17 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 import './collection.styles.scss';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 
-const CollectionPage = ({ collection }) => (
-  <div className='collection'>
-    <h2>{collection.id}</h2>
-  </div>
-);
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+  return(
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>{
+        items.map(item => <CollectionItem key={item.id} item={item} />)
+      }</div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
